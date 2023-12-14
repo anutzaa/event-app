f5b1ca3e79438624ab4f970abb96faab71024f2f
VladPetoaca <148251025+VladPetoaca@users.noreply.github.com>
2023-12-14 15:01:47 +0200

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Plata</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>bilete</h1>
+    <form method="post" action="checkout.blade.php">
+    <p>Bilet</p>
+    <p><strong>10 ron</strong></p>
+    <button>Plateste</button>
+    </form>
+</body>
+</html>
