e96ec613003d4b362a26840af096cd380e9a244a
VladPetoaca <vladpetoaca@gmail.com>
2023-12-20 14:33:00 +0200

    Update services.php
    config stripe

--- config/services.php
+++ config/services.php
@@ -14,6 +14,11 @@ return [
     |
     */
 
+    'stripe' => [
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET_KEY'),
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
